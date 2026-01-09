50394d16c91e45669909f03621d9f38a35464e9a
msnba <alex.n.coviello@gmail.com>
2026-01-08 21:27:09 -0600

    fixes

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ _A simple C++ 3D renderer using OpenGL._
 - [A C++ Compiler](https://code.visualstudio.com/docs/languages/cpp#_install-a-compiler)
 - [CMake](https://cmake.org)
 - [vcpkg](https://vcpkg.io/en)
-- [Ninja](https://ninja-build.org/)
+- [Ninja](https://ninja-build.org/) (Or another build system, just switch up the configure cmd.)
 
 ### Building 🔨
 
@@ -15,13 +15,11 @@ _A simple C++ 3D renderer using OpenGL._
    - `git clone https://github.com/msnba/3d-renderer.git`
 2. CD into the new directory:
    - `cd ./3d-renderer`
-3. Install dependencies (make sure vcpkg is added to PATH):
-   - `vcpkg install`
-4. Get the cmake toolchain file path for vcpkg:
+3. Get the cmake toolchain file path for vcpkg:
    - `vcpkg integrate install`
-5. Configure project with CMake:
+4. Configure project with CMake (automatically installs dependencies):
    - `cmake -Bbuild -DCMAKE_TOOLCHAIN_FILE=/path/to/vcpkg/scripts/buildsystems/vcpkg.cmake -G "Ninja"`
-6. Build the project with CMake:
+5. Build the project with CMake:
    - `cmake --build build`
 
 ## License
--- bin/main.cpp
+++ bin/main.cpp
@@ -2,13 +2,24 @@
 #include <GLFW/glfw3.h> // ! Must be included after GLAD (due to method overriding).
 #include <iostream>
 
+void framebuffer_size_callback(GLFWwindow *window, int width, int height);
+void get_input(GLFWwindow *window);
+
 int main()
 {
+    // -- Boilerplate --
     if (!glfwInit())
         return -1;
 
     GLFWwindow *window = glfwCreateWindow(800, 600, "Window", NULL, NULL);
-    glfwMakeContextCurrent(window); // ! Window must be contextualized before GLAD initialization.
+    if (window == NULL)
+    {
+        std::cerr << "Failed to initialize Window\n";
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);                                    // ! Window must be contextualized before GLAD initialization.
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback); // Resize callback.
 
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
     {
@@ -16,8 +27,12 @@ int main()
         return -1;
     }
 
-    while (!glfwWindowShouldClose(window)) // Simple refresh loop
+    // -- Render Loop --
+
+    while (!glfwWindowShouldClose(window))
     {
+        get_input(window);
+
         glClear(GL_COLOR_BUFFER_BIT);
         glfwSwapBuffers(window);
         glfwPollEvents();
@@ -26,4 +41,14 @@ int main()
     glfwDestroyWindow(window);
     glfwTerminate();
     return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow *window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+void get_input(GLFWwindow *window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
 }
